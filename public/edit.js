2d10d4f7f2f8be314bc1eef8a29c5a577b221607
Sho Hashimoto <hashimoto@shokai.org>
2010-05-19 04:08:32 +0900

    fix

--- public/edit.js
+++ public/edit.js
@@ -1,13 +1,17 @@
 
 document.onload = loadPage(location.href+".json");
 
-function loadPage(url){
+function loadPage(){
+    var url = location.href+".json";
     $.getJSON(url,function(json) {
 	    display(json.text);
 	    return;
 	});
 };
 
+function savePage(){
+};
+
 function display(text){
     $('#edit').html(text);
-}
+};
