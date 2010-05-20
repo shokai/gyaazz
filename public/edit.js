395775d7b89a8a9642dc62f7df32f1d1014f8ce0
Sho Hashimoto <hashimoto@shokai.org>
2010-05-20 11:15:27 +0900

    [[link]]

--- public/edit.js
+++ public/edit.js
@@ -86,6 +86,7 @@ function display(){
     edit_html = ''
     for(var i = 0; i < data.lines.length; i++){
 	var line = data.lines[i];
+	line = line.replace(/\[\[(.+)\]\]/, '<a href="./$1">$1</a>');
 	edit_html += '<li class="line" id="line' + i + '">' + line + '</li>';
 	$('li#line'+i).die('click');
 	$('body').unbind('click');
